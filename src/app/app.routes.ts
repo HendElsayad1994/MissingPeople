3ba36c2fd8380046a75c74a3cf8449abfae343c6
HendElsayad1994 <hendelsayad@gmail.com>
2017-06-08 14:34:25 +0200

    last edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import{LoginComponent} from "app/login/login.component";
 
 
 export const App_Routes:Routes=[
-<<<<<<< HEAD
+
     {path:"Login",component:LoginComponent},
     {path:"Login/:id",component:ChildrenComponent},
     {path:"children" ,component:ChildrenComponent},
@@ -14,11 +14,6 @@ export const App_Routes:Routes=[
     
     {path:"**" ,component:LoginComponent},
     //gmvm,b,hbj
-=======
-    
-  
-    
-  
->>>>>>> e12223fb71d9d49d6f74864d0d0175d3fb7bb971
+
     
 ]
