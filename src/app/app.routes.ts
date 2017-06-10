2b86f3ff8405a91af79a59d7347f46cdc1f94c42
HendElsayad1994 <hendelsayad@gmail.com>
2017-06-10 10:51:42 +0200

    last edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import{Routes} from "@angular/router";
-import{ChildInfoComponent} from "app/child-info/child-info.component";
+/*import{ChildInfoComponent} from "app/child-info/child-info.component";*/
 import{ChildrenComponent} from "app/children/children.component";
 import{LoginComponent} from "app/login/login.component";
 
@@ -9,8 +9,8 @@ export const App_Routes:Routes=[
     {path:"Login",component:LoginComponent},
     {path:"Login/:id",component:ChildrenComponent},
     {path:"children" ,component:ChildrenComponent},
-    {path:"children/:id",component:ChildInfoComponent},
-    {path:"child-info/:id",component:ChildInfoComponent},
+   /* {path:"children/:id",component:ChildInfoComponent},
+    {path:"child-info/:id",component:ChildInfoComponent}*/,
     
     {path:"**" ,component:LoginComponent},
     //gmvm,b,hbj
